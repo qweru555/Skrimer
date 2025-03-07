b568ac6ecb3815fa1abaaccca5efe005f14d7205
qweru555 <lkoelvlvlvv@gmail.com>
2025-03-07 16:47:11 +0300

    Add files via upload

---
+++ тудо.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Главные Сылки</title>
+<style>
+.product {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+.product:hover {
+  background-color: #f0f0f0;
+}
+</style>
+</head>
+<body>
+
+<h1>Наш ассортимент</h1>
+
+<div class="product" onclick="window.location.href='https://t.me/caseklikerrazdacha'">
+  <h2>Товар 1</h2>
+  <p>Канал Разраба1</p>
+</div>
+
+<div class="product" onclick="window.location.href='https://www.example.com/product2'">
+  <h2>Товар 2</h2>
+  <p>Описание товара 2</p>
+</div>
+
+<div class="product" onclick="window.location.href='https://www.example.com/product3'">
+  <h2>Товар 3</h2>
+  <p>Описание товара 3</p>
+</div>
+
+</body>
+</html>
